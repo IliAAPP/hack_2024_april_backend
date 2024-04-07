ec7f17ae4b9a2f052be0e7db7f88d45f8258a99e
IliAAPP <125664015+IliAAPP@users.noreply.github.com>
2024-04-07 12:50:31 +0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -4,12 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Role(Base):
     __tablename__ = 'roles'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     permissions = Column(JSON)
+    id_trip = Column(Integer, ForeignKey('trips.trip_id'))
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -20,3 +23,12 @@ class User(Base):
     password = Column(String, nullable=False)
     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
     role_id = Column(Integer, ForeignKey('roles.id'))
+
+
+class Trip(Base):
+    __tablename__ = 'trips'
+
+    trip_id = Column(Integer, primary_key=True)
+    trip_name = Column(String, nullable=False)
+    trip_desc = Column(String, nullable=False)
+    max_people_to_visit = Column(Integer, nullable=False)
